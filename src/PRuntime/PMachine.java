2eb8de58ab787f88046d547fdacbab0c59fa0926
richardnnn <yifanning@berkeley.edu>
2020-05-02 01:08:28 -0700

    added types, machine model in progress

--- src/PRuntime/PMachine.java
+++ src/PRuntime/PMachine.java
@@ -1,5 +1,35 @@
 package PRuntime;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class PMachine {
-    
+    private HashSet<PMachineState> stateSet;
+    private LinkedList<PMachineVar> varList;
+
+
+    private class PMachineState {
+        private boolean isStart;
+        private String name;
+
+        public void entry(PEvent event, PMachineState state) {
+            
+        }
+
+        public void onGoto(PEvent event, PMachineState state) {
+
+        }
+
+    }
+
+    private class PMachineVar {
+        private PRuntimeType type;
+        private String name;
+
+        public PMachineVar(PRuntimeType type, String name) {
+            this.type = type;
+            this.name = name;
+        }
+
+    }
 }
---
+++ src/PRuntime/PRuntimeType.java
@@ -0,0 +1,14 @@
+package PRuntime;
+
+public enum PRuntimeType {
+    INT, // PrimitiveType
+    FLOAT, // PrimitiveType
+    BOOL, // PrimitiveType
+    STRING, // PrimitiveType
+    EVENT, // PrimitiveType
+    MACHINE, // PrimitiveType
+    DATA, // PrimitiveType
+    ANY, // PrimitiveType
+    MAP, SEQ, SET, TUPLE,
+
+}
